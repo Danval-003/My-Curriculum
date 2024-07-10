9bc7f05d25f9b689762757aeffa0b6070ed207ad
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2024-07-09 18:43:31 -0600

    Add more projects

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,11 +2,19 @@ import EverythingStarsWithWater from './Images/EverythingSWW.webp'
 import SocialNetwork from './Images/Grafo.webp'
 import SLR from './Images/SLR.webp'
 import Danvaland from './Images/danvaland.webp'
+import ChatTCP from './Images/chatTCP.webp'
+import Raycasting from './Images/Raycasting.webp'
+import Raycaster from './Images/raycaster.webp'
+import Bline from './Images/Bline.webp'
 
 
 export {
     EverythingStarsWithWater,
     SocialNetwork,
     SLR,
-    Danvaland
+    Danvaland,
+    ChatTCP,
+    Raycasting,
+    Raycaster,
+    Bline
 }
--- src/pages/ToShow/ToShow.tsx
+++ src/pages/ToShow/ToShow.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { AboutMe, Experience } from '../../sections'
 import { useTranslation } from "react-i18next";
-import { SocialNetwork, EverythingStarsWithWater, SLR, Danvaland } from '../../assets';
+import { 
+  SocialNetwork, 
+  EverythingStarsWithWater, 
+  SLR, 
+  Danvaland, 
+  Raycasting, 
+  ChatTCP,
+  Raycaster,
+  Bline,
+} from '../../assets';
 import './ToShow.scss'
 
 interface ProjectData {
@@ -36,7 +45,31 @@ const ToShow: React.FC = () => {
       title: t("projects.danvaland.title"),
       description: t("projects.danvaland.description"),
       image: Danvaland,
-      link: "https://github.com/Danval-003/LexicalAnalyzer-LL1-SRL-Scanner"
+      link: "https://danvaland.danval.lat/"
+    },
+    {
+      title: t("projects.TCP_chat.title"),
+      description: t("projects.TCP_chat.description"),
+      image: ChatTCP,
+      link: "https://github.com/Danval-003/TCP_chat_server"
+    },
+    {
+      title: t("projects.Doom_raycasting.title"),
+      description: t("projects.Doom_raycasting.description"),
+      image: Raycasting,
+      link: "https://github.com/Danval-003/DoomProyectXP"
+    },
+    {
+      title: t("projects.Raycaster.title"),
+      description: t("projects.Raycaster.description"),
+      image: Raycaster,
+      link: "https://github.com/Danval-003/Minecraft_for_raycaster"
+    },
+    {
+      title: t("projects.B-line.title"),
+      description: t("projects.B-Line.description"),
+      image: Bline,
+      link: "https://github.com/Dahernandezsilve/Project_B-Line"
     },
   ]
   return (
